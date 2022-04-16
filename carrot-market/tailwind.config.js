417bd6db7d0b279bd3035842bb8b989b73dec89f
20140108 <rlarbxo157@naver.com>
2022-04-16 23:29:15 +0900

    make login form

--- carrot-market/tailwind.config.js
+++ carrot-market/tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
     content: [
-        './pages/**/*.{html,ts,tsx}',
-        './components/**/*.{html,ts,tsx}',
+        './pages/**/*.{html,ts,tsx,js,jsx}',
+        './components/**/*.{html,ts,tsx,js,jsx}',
     ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode:"media",
+  plugins: [require("@tailwindcss/forms")],
 }
+
 // 어디에서 tailwind 를 사용할것인지 알려줘야함.
 // 예를 들어 pages 폴더에서 사용할것이면 content:[] 에 ./pages/**/*.{}  -> pages 폴더에 모든 폴더에 모든파일이 가능, 확장자명 제한
