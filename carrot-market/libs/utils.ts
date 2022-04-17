bc57ac6c8a0ed152d257c5d651809fa1369f249c
20140108 <rlarbxo157@naver.com>
2022-04-17 20:26:11 +0900

    front update [main,profile,item]

---
+++ carrot-market/libs/utils.ts
@@ -0,0 +1,3 @@
+export default function cls(...classNames:string[]) {
+    return classNames.join(" ");
+}
--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -1,30 +1,42 @@
-import type {NextPage} from 'next'
+import type { NextPage } from "next";
+import FloatingButton from "../components/floating-button";
+import Item from "../components/item";
+import Layout from "../components/layout";
 
 const Home: NextPage = () => {
-    return (
-        <div className={"bg-slate-400 py-20 px-10 grid gap-10"}>
-            <div className={"bg-white p-6 rounded-3xl shadow-xl"}>
-                <span className="font-semibold text-3xl my-2">Select Item</span>
-                <div className='flex justify-between my-2'>
-                    <span className='text-gray-500'>Grey Chair</span>
-                    <span className="font-semibold">$19</span>
-                </div>
-                <div className='flex justify-between'>
-                    <span>Grey Chair</span>
-                    <span className="font-semibold">$19</span>
-                </div>
-                <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed'>
-                    <span>Total</span>
-                    <span className="font-semibold">$10</span>
-                </div>
-                <div className='mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto'>Checkout</div>
-                    
-            </div>
-            <div className={"bg-white p-10 rounded-2xl shadow-xl"}></div>
-            <div className={"bg-white p-10 rounded-2xl shadow-xl"}></div>
-            <div className={"bg-white p-10 rounded-2xl shadow-xl"}></div>
-        </div> 
-    )
-}
+  return (
+    <Layout title="홈" hasTabBar>
+      <div className="flex flex-col space-y-5 divide-y">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item
+            id={i}
+            key={i}
+            title="iPhone 14"
+            price={99}
+            comments={1}
+            hearts={1}
+          />
+        ))}
+        <FloatingButton href="/items/upload">
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </FloatingButton>
+      </div>
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
